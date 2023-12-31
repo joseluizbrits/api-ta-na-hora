d5909ab38f951e6589b4eff0c0a8626cf1889c59
brits <joseluizbrits@gmail.com>
2023-12-31 20:44:15 -0300

    modifying schema

--- src/models/Watch.ts
+++ src/models/Watch.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: {
+  model: {
     type: String,
     required: true,
   },
@@ -11,6 +11,26 @@ const schema = new mongoose.Schema({
   },
   price: {
     type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  box: {
+    type: String,
+    required: true,
+  },
+  bracelet: {
+    type: String,
+    required: true,
+  },
+  identifier: {
+    type: String,
     required: true,
   },
 });
